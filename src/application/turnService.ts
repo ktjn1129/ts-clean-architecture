467bdc95d8b6dbb576a9b3b153245d104005aeea
ktjn1129 <ktjn1129@gmail.com>
2024-02-10 19:59:12 +0900

    add validation

--- src/application/turnService.ts
+++ src/application/turnService.ts
@@ -1,5 +1,5 @@
 import { GameRepository } from "../domain/model/game/gameRepository";
-import { toDisc } from "../domain/model/turn/disc";
+import { Disc, toDisc } from "../domain/model/turn/disc";
 import { Point } from "../domain/model/turn/point";
 import { TurnRepository } from "../domain/model/turn/turnRepository";
 import { connectMySQL } from "../infrastructure/connection";
@@ -70,7 +70,7 @@ export class TurnService {
     }
   }
 
-  async registerTurn(turnCount: number, disc: number, x: number, y: number) {
+  async registerTurn(turnCount: number, disc: Disc, point: Point) {
     const connection = await connectMySQL();
 
     try {
@@ -97,7 +97,7 @@ export class TurnService {
       );
 
       //石を置く
-      const newTurn = previousTurn.placeNext(toDisc(disc), new Point(x, y));
+      const newTurn = previousTurn.placeNext(disc, point);
 
       //ターンを保存する
       await turnRepository.save(connection, newTurn);
